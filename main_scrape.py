c401313725d591335c3d67f8d68d3459a2ca2fdb
Matthew Tan <128443789+matthewtan01@users.noreply.github.com>
2024-07-18 12:57:40 +0800

    added scrape option

--- main_scrape.py
+++ main_scrape.py
@@ -1,28 +1,27 @@
 import os
 import requests
 from dotenv import load_dotenv
-from telegram import Update, Bot
+from bs4 import BeautifulSoup
+from telegram import Update
 from telegram.ext import CommandHandler, CallbackContext, Application, ContextTypes
 
 
 load_dotenv()
-vantage_api = os.getenv("vantage_api").strip()
 telegram_bot_token = os.getenv("bot_token").strip()
+scrape_url = os.getenv("scrape_url").strip()
 subscribed_users = set()
 
 
-def get_swedish_data():
-    url = f'https://www.alphavantage.co/query?function=CURRENCY_EXCHANGE_RATE&from_currency=SGD&to_currency=SEK&apikey={vantage_api}'
-    r = requests.get(url)
-    data = r.json()
-    return data
+def scrape_swedish_data():
+    response = requests.get(scrape_url)
+    soup = BeautifulSoup(response.text, features="html.parser")
+    swedish_input_field = soup.find('span', class_="text-success")
+    return swedish_input_field.text
 
 
 async def send_announcement(context: ContextTypes.DEFAULT_TYPE):
     print("Sending annoucement")
-    data = get_swedish_data()
-    exchange_rate = data['Realtime Currency Exchange Rate']['5. Exchange Rate']
-    exchange_rate = round(float(exchange_rate), 2)
+    exchange_rate = scrape_swedish_data()
     text = f"1 SGD equals to {exchange_rate} SEK"
     for chat_id in subscribed_users:
         url = f"https://api.telegram.org/bot{telegram_bot_token}/sendMessage?chat_id={chat_id}&text={text}"
@@ -34,7 +33,9 @@ async def subscribe(update: Update, context: CallbackContext):
     print(f"{update.message.chat_id} subscribe")
     if chat_id not in subscribed_users:
         subscribed_users.add(chat_id)
+        rate = scrape_swedish_data()
         await update.message.reply_text("You have subscribed to receive announcements.")
+        await update.message.reply_text(f"1 SGD equals to {rate} SEK")
     else:
         await update.message.reply_text("You are already subscribed to receive annoucements.")
 
